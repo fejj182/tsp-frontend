4a469375c1d373acc786f8aed0035d4caf5111e7
Jeff Zinger <jzinger100@gmail.com>
2019-11-09 10:53:46 +0100

    TSP-1: Introduce marker module to store

---
+++ src/store/modules/markers.js
@@ -0,0 +1,9 @@
+export const state = {
+  activeMarker: {}
+};
+
+export const getters = {};
+
+export const actions = {};
+
+export const mutations = {};
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import * as markers from "@/store/modules/markers";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {
+    markers
+  }
 });
