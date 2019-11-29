6906047b29681cfa4cb2bfaf8aed176fa5d52491
Jeff Zinger <jzinger100@gmail.com>
2019-11-29 21:13:37 +0100

    TSP-0: Add vuetify

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/modules/home/Home.vue";
+import HelloWorld from "@/components/HelloWorld";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "home",
     component: Home
+  },
+  {
+    path: "/vuetify",
+    name: "vuetify",
+    component: HelloWorld
   }
 ];
 
