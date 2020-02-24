1d9c5db44a3150e01c895edf8519846da7fa5059
Jeff Zinger <jzinger100@gmail.com>
2020-02-24 19:57:17 +0100

    TSP-0: Add not found component

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/modules/home/Home.vue";
+import NotFound from "@/components/NotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -14,6 +15,10 @@ const routes = [
     path: "/trip/:alias",
     name: "alias",
     component: Home
+  },
+  {
+    path: "/*",
+    component: NotFound
   }
 ];
 
