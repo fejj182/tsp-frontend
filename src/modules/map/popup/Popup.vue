625625644b3e66319fa2ed8642b74fa8326a9e6d
Jeff Zinger <jzinger100@gmail.com>
2020-03-29 18:51:37 +0200

    TSP-2-3: Update duration string spaces

--- src/modules/map/popup/Popup.vue
+++ src/modules/map/popup/Popup.vue
@@ -53,12 +53,12 @@ export default {
     duration() {
       const duration = this.station.duration;
       const hours = Math.floor(duration / 60);
-      const hoursString = hours > 0 ? hours + "h" : "";
+      const hoursString = hours > 0 ? hours + "h " : "";
 
       const minutes = duration % 60;
-      const minutesString = minutes > 0 ? minutes : "";
+      const minutesString = minutes > 0 ? minutes + "m" : "";
 
-      return "(" + hoursString + " " + minutesString + ")";
+      return hoursString + minutesString;
     }
   },
   methods: {
