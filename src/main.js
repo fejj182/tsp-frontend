da774c83c5068c915e22b267afbbeb1326a67df9
Jeff Zinger <jzinger100@gmail.com>
2020-04-12 14:40:14 +0200

    TSP-0: Add google analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
+import VueGtag from "vue-gtag";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
 import vuetify from "./plugins/vuetify";
 
+Vue.use(VueGtag, {
+  config: { id: process.env.VUE_APP_GA_TRACKING_ID }
+});
 Vue.config.productionTip = false;
 
 new Vue({
