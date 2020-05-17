0541d32c6521c33264a1b88504dea693fbbfa70d
Jeff Zinger <jzinger100@gmail.com>
2020-05-17 20:32:24 +0200

    TSP-0: Add min/max zoom to map

--- src/modules/map/Map.vue
+++ src/modules/map/Map.vue
@@ -27,7 +27,9 @@ export default {
       tileOptions: {
         attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
+        minZoom: 5,
+        // TODO: minZoom 6 causes big performance issues
+        maxZoom: 10,
         id: "mapbox.streets",
         accessToken: process.env.VUE_APP_OPEN_STREET_MAPS_KEY
       },
