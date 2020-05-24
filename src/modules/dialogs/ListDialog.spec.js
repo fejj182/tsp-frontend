52bd44b65d3384e61c15462a0fd0ba8ac22074fb
Jeff Zinger <jzinger100@gmail.com>
2020-05-24 15:15:07 +0200

    TSP-3-13: Fix mock stubs in tests

--- src/modules/dialogs/ListDialog.spec.js
+++ src/modules/dialogs/ListDialog.spec.js
@@ -1,10 +1,22 @@
 import { shallowMount } from "@vue/test-utils";
+import Vue from "vue";
+import Vuetify from "vuetify";
 import ListDialog from "./ListDialog";
 import TripPanel from "@/modules/trip-panel/TripPanel";
 
+Vue.use(Vuetify);
+
 describe("ListDialog", () => {
   it("should contain TripPanel", () => {
-    const wrapper = shallowMount(ListDialog);
+    const mockStubs = {
+      VDialog: {
+        name: "v-dialog",
+        template: "<span><slot></slot></span>"
+      }
+    };
+    const wrapper = shallowMount(ListDialog, {
+      stubs: mockStubs
+    });
     expect(wrapper.find(TripPanel).exists()).toBe(true);
   });
 });
--- src/modules/header/Header.spec.js
+++ src/modules/header/Header.spec.js
@@ -1,41 +1,69 @@
 import { shallowMount } from "@vue/test-utils";
+import Vue from "vue";
+import Vuetify from "vuetify";
+
 import Header from "./Header";
 import ListDialog from "@/modules/dialogs/ListDialog";
 import HelpDialog from "@/modules/dialogs/HelpDialog";
 
+Vue.use(Vuetify);
+
 describe("Header", () => {
+  const mockStubs = {
+    VAppBar: {
+      name: "v-app-bar",
+      template: "<span><slot></slot></span>"
+    },
+    VSpacer: {
+      name: "v-spacer",
+      template: "<span></span>"
+    }
+  };
+
   beforeEach(() => {
     window.innerWidth = 1000;
   });
   it("should contain logo as a link", () => {
-    const wrapper = shallowMount(Header);
+    const wrapper = shallowMount(Header, {
+      mocks: mockStubs
+    });
     expect(wrapper.find("a > img").exists()).toBe(true);
   });
 
   it("should show h1", () => {
-    const wrapper = shallowMount(Header);
+    const wrapper = shallowMount(Header, {
+      mocks: mockStubs
+    });
     expect(wrapper.find("h1").isVisible()).toBe(true);
   });
 
   it("should hide h1 on mobile ", () => {
     window.innerWidth = 500;
-    const wrapper = shallowMount(Header);
+    const wrapper = shallowMount(Header, {
+      mocks: mockStubs
+    });
     expect(wrapper.find("h1").isVisible()).toBe(false);
   });
 
   it("should not contain ListDialog on desktop", () => {
-    const wrapper = shallowMount(Header);
+    const wrapper = shallowMount(Header, {
+      mocks: mockStubs
+    });
     expect(wrapper.find(ListDialog).exists()).toBe(false);
   });
 
   it("should contain ListDialog on mobile", () => {
     window.innerWidth = 500;
-    const wrapper = shallowMount(Header);
+    const wrapper = shallowMount(Header, {
+      mocks: mockStubs
+    });
     expect(wrapper.find(ListDialog).exists()).toBe(true);
   });
 
   it("should contain HelpDialog", () => {
-    const wrapper = shallowMount(Header);
+    const wrapper = shallowMount(Header, {
+      mocks: mockStubs
+    });
     expect(wrapper.find(HelpDialog).exists()).toBe(true);
   });
 });
