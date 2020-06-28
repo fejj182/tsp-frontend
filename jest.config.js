19ae30ccd56c1b66719346d494deca8391c532c5
Jeff Zinger <jzinger100@gmail.com>
2020-06-28 10:52:57 +0200

    TSP-!!: Upgrade Vue 4.4.6

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-  testPathIgnorePatterns: ["<rootDir>/tests/e2e"]
+  testPathIgnorePatterns: ["<rootDir>/tests/e2e"],
+  transformIgnorePatterns: ["/node_modules/(?!babel)"]
 };
