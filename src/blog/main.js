2758a21d9abbf09d0f2100b0eea66b7077c9f81d
Jeff Zinger <jzinger100@gmail.com>
2020-07-25 12:06:47 +0200

    TSP-4-10: Configure multi-page mode to add Blog decoupled from SPA

---
+++ src/blog/main.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import Blog from "./Blog";
+
+new Vue({
+  render: h => h(Blog)
+}).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,19 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
 
 module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    },
+    blog: {
+      entry: "src/blog/main.js",
+      template: "public/blog.html",
+      //TODO: Do we need all chunk-vendors here?
+      chunks: ["chunk-vendors", "chunk-common", "blog"]
+    }
+  },
   transpileDependencies: ["vuetify"],
   configureWebpack: {
     plugins: [
