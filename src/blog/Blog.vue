519df058bea78028afce4a6eafd0c1d84de2f2c0
Jeff Zinger <jzinger100@gmail.com>
2020-07-26 15:28:39 +0200

    TSP-4-10: Add BlogHeader component

--- src/blog/Blog.vue
+++ src/blog/Blog.vue
@@ -1,11 +1,55 @@
 <template>
   <div>
-    <p>Coming Soon</p>
+    <component :blok="story.content" :is="story.content.component"></component>
   </div>
 </template>
 
 <script>
-export default {};
+import StoryblokClient from "storyblok-js-client";
+
+const token = process.env.VUE_APP_STORYBLOK_TOKEN;
+
+let storyapi = new StoryblokClient({
+  accessToken: token
+});
+
+export default {
+  data() {
+    return {
+      story: {
+        content: {
+          component: null
+        }
+      }
+    };
+  },
+  created() {
+    window.storyblok.init({
+      accessToken: token
+    });
+    window.storyblok.on("change", () => {
+      this.getStory("home", "draft");
+    });
+    window.storyblok.pingEditor(() => {
+      if (window.storyblok.isInEditor()) {
+        this.getStory("home", "draft");
+      } else {
+        this.getStory("home", "published");
+      }
+    });
+  },
+  methods: {
+    getStory(slug, version) {
+      storyapi
+        .get("cdn/stories/" + slug, {
+          version: version
+        })
+        .then(response => {
+          this.story = response.data.story;
+        });
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
--- src/blog/main.js
+++ src/blog/main.js
@@ -1,5 +1,13 @@
 import Vue from "vue";
 import Blog from "./Blog";
+import StoryblokVue from "storyblok-vue";
+import Page from "./components/Page";
+import BlogHeader from "./components/BlogHeader";
+
+Vue.component("page", Page);
+Vue.component("BlogHeader", BlogHeader);
+
+Vue.use(StoryblokVue);
 
 new Vue({
   render: h => h(Blog)
