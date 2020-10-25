a26bdbfd62dfaecf386db70813ed3d3703a90cce
Jeff Zinger <jzinger100@gmail.com>
2020-10-25 16:36:45 +0000

    TSP-5-12: Remove blog from repo

--- vue.config.js
+++ vue.config.js
@@ -3,19 +3,6 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
 const path = require("path");
 
 module.exports = {
-  pages: {
-    index: {
-      entry: "src/main.js",
-      template: "public/index.html",
-      chunks: ["chunk-vendors", "chunk-common", "index"]
-    },
-    blog: {
-      entry: "blog/main.js",
-      template: "public/blog.html",
-      //TODO: Can we reduce the amount of vendors?
-      chunks: ["chunk-vendors", "chunk-common", "blog"]
-    }
-  },
   transpileDependencies: ["vuetify"],
   configureWebpack: {
     plugins: [
@@ -25,8 +12,7 @@ module.exports = {
     ],
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "src/"),
-        "@blog": path.resolve(__dirname, "blog/")
+        "@": path.resolve(__dirname, "src/")
       }
     }
   }
